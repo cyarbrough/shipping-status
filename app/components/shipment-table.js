46e1d8469bce7b39ddd1117d4e50183d0df7be5c
C. Yarbrough <cyarbrough@educationalmeasures.com>
2017-01-24 15:22:39 -0500

    Added filter logic

--- app/components/shipment-table.js
+++ app/components/shipment-table.js
@@ -4,5 +4,27 @@ export default Ember.Component.extend({
     classNames: ['shipment-table'],
     tagName: 'table',
 
-    filterValue: null
+    items: null,
+
+    filterBy: 'shippingReferenceId',
+    filterValue: null,
+
+    /**
+     * Filtered items; Will return all items if no filter
+     * @var {*}
+     */
+    filteredItems: Ember.computed('items', 'filterBy', 'filterValue', function () {
+        var items = this.get('items'),
+            filterBy = this.get('filterBy'),
+            filterValue = this.get('filterValue'),
+            regEx = new RegExp(filterValue);
+
+        if(!filterBy || !filterValue) {
+            return items;
+        }
+
+        return items.filter(function(item) {
+            return regEx.test(item.get(filterBy).toLowerCase());
+        });
+    }),
 });
